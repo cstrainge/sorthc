85063e76acdbfcf56b0b78bb37b396db8b99ef2a
Chloë Strainge <nullptr.0@gmail.com>
2025-01-03 17:09:19 -0800

    Manage the command line arguments.

--- sorth-runtime/abi/words/regiser-words.cpp
+++ sorth-runtime/abi/words/regiser-words.cpp
@@ -17,6 +17,27 @@ namespace sorth::run_time::abi::words
 {
 
 
+    namespace
+    {
+
+
+        // Store the command line arguments in an array variable so that they can be accessed by
+        // the compiled Forth program.
+        data_structures::Value argument_array;
+
+
+        // Push the gathered command line arguments onto the data stack.
+        uint8_t word_get_arguments()
+        {
+            stack_push(&argument_array);
+
+            return 0;
+        }
+
+
+    }
+
+
 
     // Called by the sorthc compiler to pre-register all of the native words that are available to
     // the user's Forth code from the run-time..
@@ -32,14 +53,23 @@ namespace sorth::run_time::abi::words
         register_structure_words(registrar);
         register_terminal_words(registrar);
         register_value_type_words(registrar);
+
+        registrar("sorth.args", "word_get_arguments");
     }
 
 
 
-    // Called at run-time to make the command line arguments available to the Forth program.
+    // Called at run-time startup to make the command line arguments available to the Forth program.
     void register_command_line_arguments(int argc, char* argv[])
     {
-        //
+        auto new_array = std::make_shared<data_structures::Array>(argc);
+
+        for (int i = 0; i < argc; ++i)
+        {
+            (*new_array)[i] = argv[i];
+        }
+
+        argument_array = new_array;
     }
 
 
