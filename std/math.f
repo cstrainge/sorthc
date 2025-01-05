3d74baf3c5f0eea2d9a177ce3dba625be3cb0eee
Chloë Strainge <nullptr.0@gmail.com>
2025-01-05 14:58:31 -0800

    Added some math functions.

---
+++ std/math.f
@@ -0,0 +1,72 @@
+
+( Bring in some C library math functions. )
+
+
+
+ffi.fn acos as math.acos ffi.f64 -> ffi.f64
+
+
+ffi.fn asin as math.asin ffi.f64 -> ffi.f64
+
+
+ffi.fn atan as math.atan ffi.f64 -> ffi.f64
+
+
+ffi.fn atan2 as math.atan2 ffi.f64 ffi.f64 -> ffi.f64
+
+
+ffi.fn cos as math.cos ffi.f64 -> ffi.f64
+
+
+ffi.fn cosh as math.cosh ffi.f64 -> ffi.f64
+
+
+ffi.fn sin as math.sin ffi.f64 -> ffi.f64
+
+
+ffi.fn sinh as math.sinh ffi.f64 -> ffi.f64
+
+
+ffi.fn tan as math.tan ffi.f64 -> ffi.f64
+
+
+ffi.fn tanh as math.tanh ffi.f64 -> ffi.f64
+
+
+ffi.fn exp as math.exp ffi.f64 -> ffi.f64
+
+
+ffi.fn frexp as math.frexp ffi.f64 -> ffi.f64
+
+
+ffi.fn ldexp as math.ldexp ffi.f64 ffi.i32 -> ffi.f64
+
+
+ffi.fn log as math.log ffi.f64 -> ffi.f64
+
+
+ffi.fn log10 as math.log10 ffi.f64 -> ffi.f64
+
+
+ffi.fn modf as math.modf ffi.f64 -> ffi.f64
+
+
+ffi.fn pow as math.pow ffi.f64 ffi.f64 -> ffi.f64
+
+
+ffi.fn sqrt as math.sqrt ffi.f64 -> ffi.f64
+
+
+ffi.fn ceil as math.ceil ffi.f64 -> ffi.f64
+
+
+ffi.fn fabs as math.fabs ffi.f64 -> ffi.f64
+
+
+ffi.fn floor as math.floor ffi.f64 -> ffi.f64
+
+
+ffi.fn fmod as math.fmod ffi.f64 ffi.f64 -> ffi.f64
+
+
+ffi.fn round as math.round ffi.f64 -> ffi.f64
